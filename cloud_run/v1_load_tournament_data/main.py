08b64c46a2a2d42637d05a8315f5838f58cb627e
Michael Koch <thekizoch@gmail.com>
2021-11-01 15:08:30 -0500

    working local v2_load_tournament_data

--- cloud_run/v1_load_tournament_data/main.py
+++ cloud_run/v1_load_tournament_data/main.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from modules.v1_load import download_v1, load_into_bq_v1, add_mapping_v1
+from modules.v2_load import download_v2, load_into_bq_v2, add_mapping_v2
 
 app = Flask(__name__)
 
@@ -22,6 +23,18 @@ def start_v1_load():
     add_mapping_v1()
     print('func add_mapping_v1 successful')
     return "Did v1 download, load into bq, and added mapping"
+
+
+# loads v2 tournament data from Numerai to BQ with an ordered row mapping
+@app.route("/start_v2", methods=['POST'])
+def start_v2_load():
+    download_v2()
+    print('func download_v1 successful')
+    load_into_bq_v2()
+    print('func load_into_bq_v2 successful')
+    add_mapping_v2()
+    print('func add_mapping_v2 successful')
+    return "Did v2 download, load into bq, and added mapping"
 
 
 if __name__ == "__main__":
--- cloud_run/v1_load_tournament_data/modules/v2_load.py
+++ cloud_run/v1_load_tournament_data/modules/v2_load.py
@@ -1,9 +1,7 @@
 from google.cloud import bigquery
 from google.cloud import storage
-import logging
 import pandas as pd
 import os
-import shutil
 import numerapi
 
 
@@ -24,92 +22,87 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob.upload_from_filename(source_file_name)
 
     print(
-        "File {} uploaded to {} in GCS.".format(
+        "File {} uploaded to {}.".format(
             source_file_name, destination_blob_name
         )
     )
 
 
-def download_v1():
-    # delete current v1 data from BQ
+def download_v2():
+
+    # instantiate clients
     napi = numerapi.NumerAPI()
-    bqclient = bigquery.Client(project='numerai-kizoch')
     print('instantiated numerai client')
+    bqclient = bigquery.Client(project='numerai-kizoch')
 
-    table_id = "numerai-kizoch.data.tournament_data"
+    # delete old v2 tournament table
+    table_id = "numerai-kizoch.big_data.tournament_data"
     bqclient.delete_table(table_id, not_found_ok=True)  # Make an API request.
-
     print("Deleted table '{}'.".format(table_id))
-    logging.info("Deleted table '{}'.".format(table_id))
 
-    # download tournament data v1
+    # download current set v2
     current_round = napi.get_current_round()
-    dir_path_tournament = f"tmp/numerai_v1_tournament_data_{current_round}_dir/"
-    napi.download_latest_data('tournament', 'parquet', "/" + dir_path_tournament)
 
-    # define the file path in dir
-    file_path_tournament = dir_path_tournament + 'latest_numerai_tournament_data.parquet'
+    file_path_tournament = f"tmp/numerai_v2_tournament_data_{current_round}.parquet"
+    #uri_parquet = "gs://numerai-kizoch/" + file_path_tournament
 
-    # upload tournament parquet to GCS
-    upload_blob("numerai-kizoch", "/" + file_path_tournament, f"tmp/numerai_v1_tournament_data_{current_round}.parquet")
+    napi.download_dataset("numerai_tournament_data.parquet", "/" + file_path_tournament)
+    # upload to GCS
+    upload_blob("numerai-kizoch", "/" + file_path_tournament, file_path_tournament)
 
     # create mapping dataframe
-    df_map = pd.read_parquet("/" + file_path_tournament, columns=['id'])
+    df_map = pd.read_parquet("/" + file_path_tournament, columns=['era'])
+    df_map.drop(columns='era', inplace=True)
     df_map['row'] = range(len(df_map))
 
     # upload csv mapping in GCS
-    file_path_mapping = "tmp/v1_mapping_round_{}.csv".format(current_round)
-    uri_csv = "gs://numerai-kizoch/" + file_path_mapping
-    df_map.to_csv("/" + file_path_mapping, index=False)
+    file_path_mapping = "tmp/v2_mapping_round_{}.csv".format(current_round)
+    #uri_csv = "gs://numerai-kizoch/" + file_path_mapping
+    df_map.to_csv("/" + file_path_mapping)
     upload_blob('numerai-kizoch', "/" + file_path_mapping, file_path_mapping)
 
     # deletes dir to recover memory
-    shutil.rmtree(f"/tmp/numerai_v1_tournament_data_{current_round}_dir")
-    print(f'deleted dir for tournament data')
+    os.remove('/' + file_path_tournament)
+    print(f'deleted file {file_path_tournament}')
 
     # deletes file to recover memory
     os.remove('/' + file_path_mapping)
     print(f'deleted file {file_path_mapping}')
 
 
-def load_into_bq_v1():
+def load_into_bq_v2():
     # instantiate clients
     napi = numerapi.NumerAPI()
     bqclient = bigquery.Client(project='numerai-kizoch')
-
     current_round = napi.get_current_round()
 
     # define paths
-    file_path_tournament = f"tmp/numerai_v1_tournament_data_{current_round}.parquet"
+    file_path_tournament = f"tmp/numerai_v2_tournament_data_{current_round}.parquet"
     uri_parquet = "gs://numerai-kizoch/" + file_path_tournament
 
     # load tournament parquet into BQ
-    table_id = "numerai-kizoch.data.tournament_data"
-
+    table_id = "numerai-kizoch.big_data.tournament_data"
     job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.PARQUET, \
-                                        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE)
-    uri = uri_parquet
+                                    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE)
 
     load_job = bqclient.load_table_from_uri(
-        uri, table_id, job_config=job_config
+      uri_parquet, table_id, job_config=job_config
     )  # Make an API request.
 
     # wait for job to complete
     load_job.result()
 
-
-
-def add_mapping_v1():
+def add_mapping_v2():
     # instantiate clients
     napi = numerapi.NumerAPI()
     bqclient = bigquery.Client(project='numerai-kizoch')
 
     current_round = napi.get_current_round()
-    file_path_mapping = "tmp/v1_mapping_round_{}.csv".format(current_round)
+    file_path_mapping = "tmp/v2_mapping_round_{}.csv".format(current_round)
     uri_csv = "gs://numerai-kizoch/" + file_path_mapping
 
     # add new column 'row'
-    table_id = "numerai-kizoch.data.tournament_data"
+    table_id = "numerai-kizoch.big_data.tournament_data"
 
     table = bqclient.get_table(table_id)  # Make an API request.
 
@@ -121,51 +114,45 @@ def add_mapping_v1():
     table = bqclient.update_table(table, ["schema"])  # Make an API request.
 
     if len(table.schema) == len(original_schema) + 1 == len(new_schema):
-        print("A new column has been added.")
+      print("A new column has been added.")
     else:
-        print("The column has not been added.")
+      print("The column has not been added.")
 
-    # create temp table with id, row
-    table_id = "data.temp"
+    # create temp table with row and id
+    table_id = "big_data.temp"
 
     job_config = bigquery.LoadJobConfig(
-        schema=[
-            bigquery.SchemaField("id", "STRING"),
-            bigquery.SchemaField("row", "NUMERIC"),
-        ],
-        skip_leading_rows=1,
-        # The source format defaults to CSV, so the line below is optional.
-        source_format=bigquery.SourceFormat.CSV,
+      schema=[
+          bigquery.SchemaField("id", "STRING"),
+          bigquery.SchemaField("row", "NUMERIC"),
+      ],
+      skip_leading_rows=1,
+      # The source format defaults to CSV, so the line below is optional.
+      source_format=bigquery.SourceFormat.CSV,
     )
 
     uri = uri_csv
 
     load_job = bqclient.load_table_from_uri(
-        uri, table_id, job_config=job_config)  # Make an API request.
+      uri, table_id, job_config=job_config)  # Make an API request.
 
     load_job.result()  # Waits for the job to complete.
 
     destination_table = bqclient.get_table(table_id)  # Make an API request.
     print("Loaded {} rows in table {}".format(destination_table.num_rows, \
-                                              table_id))
+                                            table_id))
 
     # join temp table into tournament table
     sql = """
-      UPDATE data.tournament_data d
+      UPDATE big_data.tournament_data d
       SET row = t.row
-      FROM data.temp t
+      FROM big_data.temp t
       WHERE d.id = t.id
         """
     query_job = bqclient.query(sql)
-    query_job.result()  # waits for job to finish
+    query_job.result() # waits for job to finish
 
     # delete temp table
-    table_id = "numerai-kizoch.data.temp"
+    table_id = "numerai-kizoch.big_data.temp"
     bqclient.delete_table(table_id, not_found_ok=True)  # Make an API request.
     print("Deleted table '{}'.".format(table_id))
-
-
-
-
-
-
