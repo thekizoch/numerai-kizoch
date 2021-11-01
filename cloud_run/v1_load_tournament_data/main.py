7808aa08e2cf0144e0821b678a7ebadf55bfcc0d
Michael Koch <thekizoch@gmail.com>
2021-10-31 20:24:31 -0500

    added gcf weekly-cleanup

--- cloud_run/v1_load_tournament_data/main.py
+++ cloud_run/v1_load_tournament_data/main.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from modules.v1_load import download_v1, load_into_bq_v1, add_mapping_v1
 
 app = Flask(__name__)
 
@@ -9,6 +10,18 @@ app = Flask(__name__)
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
+
+
+# loads v1 tournament data from Numerai to BQ with an ordered row mapping
+@app.route("/start_v1", methods=['POST'])
+def start_v1_load():
+    download_v1()
+    print('func download_v1 successful')
+    load_into_bq_v1()
+    print('func load_into_bq_v1 successful')
+    add_mapping_v1()
+    print('func add_mapping_v1 successful')
+    return "Did v1 download, load into bq, and added mapping"
 
 
 if __name__ == "__main__":
