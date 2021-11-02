c511bbf1e3265bd8726f4bfcc52848f05e06807f
Michael Koch <thekizoch@gmail.com>
2021-11-02 05:52:04 -0500

    v1 and v2 load predict nonensembled app

--- cloud_run/load_predict_nonensembled/main.py
+++ cloud_run/load_predict_nonensembled/main.py
@@ -2,7 +2,10 @@ import os
 
 from flask import Flask
 from modules.v1_load import download_v1, load_into_bq_v1, add_mapping_v1
+from modules.v1_predict_submit import predict_bq_v1, submit_bq_submissions_v1
 from modules.v2_load import download_v2, load_into_bq_v2, add_mapping_v2
+from modules.v2_predict_submit import predict_bq_v2, submit_bq_submissions_v2
+
 
 app = Flask(__name__)
 
@@ -15,25 +18,33 @@ def hello_world():
 
 # loads v1 tournament data from Numerai to BQ with an ordered row mapping
 @app.route("/start_v1", methods=['POST'])
-def start_v1_load():
+def start_v1():
     download_v1()
     print('func download_v1 successful')
     load_into_bq_v1()
     print('func load_into_bq_v1 successful')
     add_mapping_v1()
     print('func add_mapping_v1 successful')
+    predict_bq_v1()
+    print('func predict_bq_v1 successful')
+    submit_bq_submissions_v1()
+    print('func submit_bq_submissions_v1 successful')
     return "Did v1 download, load into bq, and added mapping"
 
 
 # loads v2 tournament data from Numerai to BQ with an ordered row mapping
 @app.route("/start_v2", methods=['POST'])
-def start_v2_load():
+def start_v2():
     download_v2()
     print('func download_v1 successful')
     load_into_bq_v2()
     print('func load_into_bq_v2 successful')
     add_mapping_v2()
     print('func add_mapping_v2 successful')
+    predict_bq_v2()
+    print('func predict_bq_v2 successful')
+    submit_bq_submissions_v2()
+    print('func submit_bq_submissions_v2 successful')
     return "Did v2 download, load into bq, and added mapping"
 
 
---
+++ cloud_run/load_predict_nonensembled/modules/v2_predict_submit.py
@@ -0,0 +1,136 @@
+from google.cloud import bigquery
+from google.cloud import storage
+import os
+import numerapi
+
+
+# Predict on all v2 BQ models func
+def predict_bq_v2():
+    # df from bq of model registry
+    bqclient = bigquery.Client()
+    df_v2_bq_models = bqclient.query("SELECT * FROM \
+    `numerai-kizoch.model_registry.v2_bq_models`").to_dataframe()
+    # Predict on all v2 BQ models
+    for index, row in df_v2_bq_models.iterrows():
+        # define model from bq table
+        MODEL_NAME = row[0]
+        MODEL_LOCATION = row[1]
+
+        # Set table id to the ID of the destination table.
+        results_table_id = "numerai-kizoch.predictions.{}".format(MODEL_NAME)
+
+        # config set to overwrite previous predictions
+        job_config = bigquery.QueryJobConfig(destination=results_table_id, \
+                                             write_disposition='WRITE_TRUNCATE')
+
+        sql = """
+    SELECT
+      id, predicted_target AS prediction
+    FROM
+      ML.PREDICT (MODEL `{}`,
+        (
+        SELECT
+          * EXCEPT (era, data_type)
+        FROM
+          `numerai-kizoch.big_data.tournament_data`
+        )
+      )
+    ORDER BY row ASC
+      """.format(MODEL_LOCATION)
+
+        # execute and wait
+        query_job = bqclient.query(sql, job_config=job_config)
+        query_job.result()
+
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your GCS object
+    # source_blob_name = "storage-object-name"
+
+    # The path to which the file should be downloaded
+    # destination_file_name = "local/path/to/file"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+    # any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Downloaded storage object {} from bucket {} to local file {}.".format(
+            source_blob_name, bucket_name, destination_file_name
+        )
+    )
+
+
+def submit_bq_submissions_v2():
+    # define environment variables
+    PROJECT = 'numerai-kizoch'
+    CURRENT_ROUND = numerapi.NumerAPI().get_current_round()
+    model_version = 2
+
+    # authentication
+    public_id = "HSOL3P7JYNV3D7IXEU56H26NXN7JETDO"
+    secret_key = os.getenv('SECRET_KEY')
+    napi = numerapi.NumerAPI(public_id=public_id, secret_key=secret_key)
+
+    # define MODEL_NAME's and MODEL_ID's
+    bqclient = bigquery.Client()
+
+    # create df of all bq models
+    df_model_registry = bqclient.query('SELECT * FROM `model_registry`.__TABLES__;').to_dataframe()
+    list_table_ids = list(df_model_registry.table_id)
+
+    for table_id in list_table_ids:
+        if 'v2_bq_models' == table_id:
+            df_bq_v2_models = bqclient.query('SELECT * FROM model_registry.{}'.format(table_id)).to_dataframe()
+
+    # write all submission.csv's to GCS, and download to local /tmp
+    for index, row in df_bq_v2_models.iterrows():
+        MODEL_NAME = row[0]
+
+        # write to GCS
+        destination_uri = "gs://{}/tmp/submissions/round_{}/submission_{}.csv".format(
+            PROJECT, CURRENT_ROUND, MODEL_NAME, )
+        results_table_id = "numerai-kizoch.predictions.{}".format(MODEL_NAME)
+        extract_job = bqclient.extract_table(
+            results_table_id,
+            destination_uri,
+            location="US")  # API request
+        extract_job.result()  # Waits for job to complete
+
+        # download
+        source_blob_name = "tmp/submissions/round_{}/submission_{}.csv".format(
+            CURRENT_ROUND, MODEL_NAME)
+        destination_file_name = '/tmp/submission_{}.csv'.format(MODEL_NAME)
+
+        download_blob(bucket_name=PROJECT, \
+                      source_blob_name=source_blob_name, \
+                      destination_file_name=destination_file_name)
+
+    # submit and delete file to recover memory
+    for index, row in df_bq_v2_models.iterrows():
+        MODEL_NAME = row[0]
+        MODEL_ID = row[2]
+
+        # local path
+        file_path_submission = f"/tmp/submission_{MODEL_NAME}.csv"
+
+        # submit
+        submission_id = napi.upload_predictions(file_path_submission, \
+                                                model_id=MODEL_ID, \
+                                                version=model_version)
+
+        print("The submission_id is {} for model {}".format(submission_id, \
+                                                            MODEL_NAME))
+        # remove local submission file to recover memory
+        os.remove(file_path_submission)
